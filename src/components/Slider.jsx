4e50b2e96650d424b1c7e416e12752b436cb3754
Rieger01 <rieger01@allegheny.edu>
2026-04-01 16:31:54 -0400

    adding slider buttons

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,22 +1,27 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 
 const Slider = ({slides}) => {
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    const autoScroll = () => {
-        if (currentIndex === slides.length -1) {
-            return setCurrentIndex(0);
+    const handleClick = (evt) => {
+        console.log(evt.target.className);
+        if(evt.target.className.includes("right")) {
+            if (currentIndex === slides.length - 1) {
+                return setCurrentIndex(0);
+            }
+            return setCurrentIndex(currentIndex + 1);
+        } else {
+            if (currentIndex === 0) {
+                return setCurrentIndex(slides.length - 1);
+            }
+            return setCurrentIndex(currentIndex - 1);
         }
-        return setCurrentIndex(currentIndex + 1);
     }
-
-    useEffect(() => {
-        const interval = setInterval(() => {autoScroll()}, 3000);
-        return () => clearInterval(interval);
-    })
 
     return(
         <div className="image-slider">
+            <button onClick = {handleClick} className="left"><i className="fa-solid fa-circle-arrow-left"></i></button>
+            <button onClick = {handleClick} className="right"><i className="fa-solid fa-circle-arrow-right"></i></button>
             <ul>
                 {slides.map((slide, index) => (
                     <li key = {index} className = {index === currentIndex ? "active" : ""}>
